134365358eaad649a0b2bbe4c7d0337b58f7de99
cgorrino <cgorrino03@gmail.com>
2021-07-27 16:13:25 -0700

    Adding a transaction to the BudgetStore

--- public/db.js
+++ public/db.js
@@ -11,7 +11,7 @@ request.onupgradeneeded = function (event) {
       const objectStore = db.createObjectStore('BudgetStore', {autoIncrement:true})
     } 
     
-  };
+};
 
   // After the Indexed DB is created and the user is online, call the checkDatabase function to transfer any stored transactions
 request.onsuccess = function (event) {
@@ -20,8 +20,18 @@ request.onsuccess = function (event) {
     if (navigator.onLine) {
       checkDatabase();
     }
-  };
+};
   
 request.onerror = function (event) {
     console.log(event.target.errorCode)
-  };
+};
+
+// Creating function to save a transation to the IndexedDB when the API call to add it to the regular DB fails
+function saveRecord(record) {
+    // create a transaction on the pending db with readwrite access
+    // access the pending object store
+    // add record to the store with add method.
+    const transaction = db.transaction(["BudgetStore"], "readwrite");
+    const BudgetStore = transaction.objectStore("BudgetStore");
+    BudgetStore.add(record);
+}
