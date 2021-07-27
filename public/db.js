cf2beb49df5e7eb8aa951dec66e09b80d41c4d3a
cgorrino <cgorrino03@gmail.com>
2021-07-27 16:00:51 -0700

    Creating object store.

--- public/db.js
+++ public/db.js
@@ -3,3 +3,12 @@ let db;
 // Create a new Indexed DB for offline operations
 
 const request = indexedDB.open('BudgetStoreDB', 1);
+
+request.onupgradeneeded = function (event) {
+    // create object store called "BudgetStore" and set autoIncrement to true to include an automatic ID
+    const db = event.target.result;
+    if(db.objectStoreNames.length === 0) {
+      const objectStore = db.createObjectStore('BudgetStore', {autoIncrement:true})
+    } 
+    
+  };
